2721ed245aaca177e3defe5fc9fdc228a83b42e3
Nafis Hasrat Arnob <arnb13@gmail.com>
2021-02-06 14:03:42 +0600

    addTeacher.dart added

---
+++ lib/page/teacher/addTeacher.dart
@@ -0,0 +1,178 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart';
+import 'package:school_flutter/page/student/studentData.dart';
+
+class AddTeacher extends StatefulWidget {
+  @override
+  AddTeacherState createState() => AddTeacherState();
+}
+
+class AddTeacherState extends State {
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController firstNameCont = TextEditingController();
+    TextEditingController lastNameCont = TextEditingController();
+    TextEditingController emailCont = TextEditingController();
+    TextEditingController phoneCont = TextEditingController();
+
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.teal,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Add New Teacher',
+            style: TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: firstNameCont,
+                keyboardType: TextInputType.name,
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                  helperText: '',
+                  helperStyle:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                  hintText: 'First Name',
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: lastNameCont,
+                keyboardType: TextInputType.name,
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                  helperText: '',
+                  helperStyle:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                  hintText: 'Last Name',
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: emailCont,
+                keyboardType: TextInputType.emailAddress,
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                  helperText: '',
+                  helperStyle:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                  hintText: 'Email Address',
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: phoneCont,
+                keyboardType: TextInputType.phone,
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                  helperText: '',
+                  helperStyle:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                  hintText: 'Phone Number',
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: RaisedButton(
+                    child: Text('Submit'),
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)),
+                    color: Colors.teal,
+                    onPressed: () {
+                      submit(firstNameCont, lastNameCont, emailCont, phoneCont,
+                          context);
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  /*
+    Submit teacher data. Email unique
+   */
+
+  void submit(
+      TextEditingController firstNameCont,
+      TextEditingController lastNameCont,
+      TextEditingController emailCont,
+      TextEditingController phoneCont,
+      BuildContext context) {
+    String firstName = firstNameCont.text;
+    String lastName = lastNameCont.text;
+    String email = emailCont.text;
+    String phone = phoneCont.text;
+
+    if (firstName.isNotEmpty &&
+        lastName.isNotEmpty &&
+        email.isNotEmpty &&
+        phone.isNotEmpty) {
+      studentData data = studentData(
+          id: 0,
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          phone: phone);
+      Future success = studentData().addStudentApi(data);
+
+      success.then((value) {
+        Response response = value;
+        if (response.statusCode == 200) {
+          Fluttertoast.showToast(
+            msg: 'New student added successfully',
+            backgroundColor: Colors.green[300],
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            textColor: Colors.black,
+          );
+
+          Navigator.pop(context);
+
+          Navigator.pushNamed(context, '/teacherProfile', arguments: {
+            'email': email,
+          });
+        } else {
+          Fluttertoast.showToast(
+            msg: jsonDecode(response.body)['status'],
+            backgroundColor: Colors.red[300],
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            textColor: Colors.black,
+          );
+        }
+      });
+    } else {
+      Fluttertoast.showToast(
+        msg: 'Please fill up all fields',
+        backgroundColor: Colors.red[300],
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        textColor: Colors.black,
+      );
+    }
+  }
+}
--- lib/page/teacher/teacherData.dart
+++ lib/page/teacher/teacherData.dart
@@ -8,12 +8,21 @@ class teacherData {
   final String lastName;
   final String email;
   final String phone;
+  final List sections;
 
   String url_all_teacher = 'http://10.0.2.2:8000/api/all_teacher';
   String url_delete_teacher = 'http://10.0.2.2:8000/api/delete_teacher';
   String url_update_teacher = 'http://10.0.2.2:8000/api/update_teacher';
+  String url_get_teacher = 'http://10.0.2.2:8000/api/get_teacher';
+  String url_add_teacher = 'http://10.0.2.2:8000/api/add_teacher';
 
-  teacherData({this.id, this.firstName, this.lastName, this.email, this.phone});
+  teacherData(
+      {this.id,
+      this.firstName,
+      this.lastName,
+      this.email,
+      this.phone,
+      this.sections});
 
   factory teacherData.fromJson(Map<String, dynamic> json) {
     return teacherData(
@@ -21,7 +30,8 @@ class teacherData {
         firstName: json['first_name'],
         lastName: json['last_name'],
         email: json['email'],
-        phone: json['phone']);
+        phone: json['phone'],
+        sections: json['section']);
   }
 
   /*
@@ -62,7 +72,7 @@ class teacherData {
   /*
     update teacher. return success bool. email unique
   */
-  Future<bool> updateTeacher(teacherData data) async {
+  Future<Response> updateTeacher(teacherData data) async {
     Response response = await post(
       url_update_teacher,
       headers: <String, String>{
@@ -76,10 +86,51 @@ class teacherData {
       }),
     );
 
+    return response;
+  }
+
+  /*
+    Get teacher profile and sections by email
+   */
+  Future<teacherData> getTeacherApi(String email) async {
+    Response response = await post(
+      url_get_teacher,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{'email': email}),
+    );
+
     if (response.statusCode == 200) {
-      return true;
+      return teacherData(
+          id: jsonDecode(response.body)['teacher']['id'],
+          firstName: jsonDecode(response.body)['teacher']['first_name'],
+          lastName: jsonDecode(response.body)['teacher']['last_name'],
+          email: jsonDecode(response.body)['teacher']['email'],
+          phone: jsonDecode(response.body)['teacher']['phone'],
+          sections: jsonDecode(response.body)['section']);
     } else {
-      return false;
+      return null;
     }
+  }
+
+  /*
+    Add new teacher. Email unique
+   */
+  Future<Response> addTeacherApi(teacherData data) async {
+    Response response = await post(
+      url_add_teacher,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'first_name': data.firstName,
+        'last_name': data.lastName,
+        'email': data.email,
+        'phone': data.phone
+      }),
+    );
+
+    return response;
   }
 }
